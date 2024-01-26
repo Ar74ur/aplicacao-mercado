9f5241e12da251fbe9cee856f7a4016bfad8a54a
Ar74ur <arthur.almeidas@upe.br>
2024-01-25 21:50:31 -0300

    Configurações iniciais do frontend

---
+++ frontend/site_mercado/src/app/page.tsx
@@ -0,0 +1,16 @@
+import { api } from "@/services/api";
+import { useState } from "react";
+
+interface Product {
+  id: number;
+  name: string;
+  preco: number;
+}
+
+export default function Home() {
+  return (
+    <main>
+
+    </main>
+  );
+}
